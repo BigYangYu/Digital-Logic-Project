ed3a32cf6d4a006cd6dd1de2d83f2978748c777a
YangYu <118981007+BigYangYu@users.noreply.github.com>
2022-12-07 23:47:18 +0800

    手动挡

--- 20022/12/13sources/ManualDrivingMode.v
+++ 20022/12/13sources/ManualDrivingMode.v
@@ -20,9 +20,8 @@
 //////////////////////////////////////////////////////////////////////////////////
 module ManualDrivingMode(
     input clk, //bind to P17 pin (100MHz system clock)
-    input rx, //bind to N5 pin
-    output tx, //bind to T4 pin
 
+    
     input rst,
     input throttle,//油门
     input clutch,//离合
@@ -33,17 +32,19 @@ module ManualDrivingMode(
     input move_backward_signal,
     input place_barrier_signal,
     input destroy_barrier_signal,
-   input[7:0]rec
+    output[7:0]rec,
+    output[7:0]answer
     );
-    reg cur;//输出复位信号
+    //输出复位信号
      wire [7:0] in = {2'b10, destroy_barrier_signal, place_barrier_signal, ~turn_right_signal, ~turn_left_signal, ~move_backward_signal, ~move_forward_signal};
      reg[1:0] state,next_state;
-    parameter  unstarting=2'b00, starting=2'b01, moving=2'b10,poweroff=2'b11;
-      always@(* ) begin
+     reg[7:0] cur=8'b10000000 ;
+    parameter unstarting=2'b00, starting=2'b01, moving=2'b10,poweroff=2'b11;
+always@(* ) 
    if(~rst)begin
             state= unstarting;
             end
-            else begin    
+            else    
           case (state)
 unstarting:if(throttle&&~clutch) next_state=poweroff;
            else if(clutch&&throttle&&~brake) next_state=starting;
@@ -57,16 +58,15 @@ moving: if(brake) next_state=unstarting;
         else if(~move_backward_signal&&~clutch)next_state=poweroff;
         else next_state=moving;
     endcase  
-  endcase
-  endcase
 //状态切换
 always@(*)begin
     case(state)
-    2'b00: cur<=1;
-     2'b01: cur<=1;
-      2'b10: cur<=1;
-       2'b11: cur<=0;
-  endcase
+    2'b00:  cur=8'b10000000;
+     2'b01: cur={ 2'b10,destroy_barrier_signal, place_barrier_signal, ~turn_right_signal, ~turn_left_signal, 2'b10 };
+      2'b10:  cur={2'b10, destroy_barrier_signal, place_barrier_signal, ~turn_right_signal, ~turn_left_signal, ~move_backward_signal, ~move_forward_signal};
+       2'b11:  cur=8'b10000000;
+endcase
     end
- 
+    assign answer=cur;
+
 endmodule
