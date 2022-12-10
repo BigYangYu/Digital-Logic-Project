a628cde5515193a5e7a53842d319a34b766d7501
YangYu <118981007+BigYangYu@users.noreply.github.com>
2022-12-10 09:51:56 +0800

    2022/12/10

--- 20022/12/13sources/ManualDrivingMode.v
+++ 20022/12/13sources/ManualDrivingMode.v
@@ -20,9 +20,8 @@
 //////////////////////////////////////////////////////////////////////////////////
 module ManualDrivingMode(
     input clk, //bind to P17 pin (100MHz system clock)
-
-    
     input rst,
+    
     input throttle,//油门
     input clutch,//离合
     input brake,
@@ -30,15 +29,14 @@ module ManualDrivingMode(
     input turn_right_signal,
     input move_forward_signal,
     input move_backward_signal,
-    input place_barrier_signal,
-    input destroy_barrier_signal,
+
     output[7:0]rec,
-    output[7:0]answer
+    output[5:0]answer,
+    output[1:0] state1
     );
     //输出复位信号
-     wire [7:0] in = {2'b10, destroy_barrier_signal, place_barrier_signal, ~turn_right_signal, ~turn_left_signal, ~move_backward_signal, ~move_forward_signal};
-     reg[1:0] state,next_state;
-     reg[7:0] cur=8'b10000000 ;
+     reg[1:0] state;
+     reg[5:0] cur=6'b100000 ;
     parameter unstarting=2'b00, starting=2'b01, moving=2'b10,poweroff=2'b11;
 always@(* ) 
    if(~rst)begin
@@ -46,27 +44,27 @@ always@(* )
             end
             else    
           case (state)
-unstarting:if(throttle&&~clutch) next_state=poweroff;
-           else if(clutch&&throttle&&~brake) next_state=starting;
-           else next_state=unstarting;
-starting:if(brake) next_state=unstarting;
-         else if(throttle&&~clutch)next_state=moving ;
-         else next_state=starting;
-moving: if(brake) next_state=unstarting;
-        else if(~throttle) next_state=starting;
-        else if(clutch) next_state=starting;
-        else if(~move_backward_signal&&~clutch)next_state=poweroff;
-        else next_state=moving;
+unstarting:if(throttle&&~clutch) state=poweroff;
+           else if(clutch&&throttle&&~brake) state=starting;
+           else state=unstarting;
+starting:if(brake) state=unstarting;
+         else if(throttle&&~clutch)state=moving ;
+         else state=starting;
+moving: if(brake) state=unstarting;
+        else if(~throttle)state=starting;
+        else if(clutch) state=starting;
+        else if(~move_backward_signal&&~clutch)state=poweroff;
+        else state=moving;
     endcase  
 //状态切换
 always@(*)begin
     case(state)
-    2'b00:  cur=8'b10000000;
-     2'b01: cur={ 2'b10,destroy_barrier_signal, place_barrier_signal, ~turn_right_signal, ~turn_left_signal, 2'b10 };
-      2'b10:  cur={2'b10, destroy_barrier_signal, place_barrier_signal, ~turn_right_signal, ~turn_left_signal, ~move_backward_signal, ~move_forward_signal};
-       2'b11:  cur=8'b10000000;
+    2'b00:  cur=6'b100000;
+     2'b01: cur={ 2'b10, ~turn_right_signal, ~turn_left_signal, 2'b10 };
+      2'b10:  cur={2'b10,  ~turn_right_signal, ~turn_left_signal, ~move_backward_signal, ~move_forward_signal};
+       2'b11:  cur=6'b100000;
 endcase
     end
     assign answer=cur;
-
+    assign state1=state;
 endmodule
