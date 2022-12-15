be5602343ca670ab6ead5c4326d532c760155437
Joy-Mine <1525323329@qq.com>
2022-12-15 11:43:38 +0800

    半自动2.0

--- 20022/12/13sources/SemiAutoDriving.v
+++ 20022/12/13sources/SemiAutoDriving.v
@@ -2,7 +2,7 @@
 
 
 module SemiAutoDriving(
-    input clk,
+    input clk, //(100MHz system clock)
     // input rst,
 
     input front_detector,
@@ -15,10 +15,8 @@ module SemiAutoDriving(
     input turn_right_command,
 
     output reg move_forward_signal,
-    // output reg move_backward_signal,
     output reg turn_left_signal,
     output reg turn_right_signal,
-    // output reg [3:0] answer,//左转，右转，后退，前进
 
     output reg [1:0]state
 );
@@ -26,13 +24,8 @@ module SemiAutoDriving(
 //Wait-for-command,Turn-Left,Turn-Right,Moving
 //00,01,10,11
 
-// move_backward_signal=1b'0;
-reg clk_50Hz;
-reg[31:0] cnt;
-clk_div_50HZ clk50(.sys_clk(clk),.turning_clk(clk_50Hz));
-reg[1:0] turning;
-//00没在转向，01正在左转，10正在右转，11转向结束
 
+reg[31:0] cnt;
 always @ (posedge clk) begin
     case(state)
         2'b11: case({front_detector,left_detector,right_detector})
@@ -43,57 +36,41 @@ always @ (posedge clk) begin
                     default: {move_forward_signal,turn_left_signal,turn_right_signal}=3'b100;
                 endcase
         2'b01: if(cnt==32'b1_000_000) begin
-                    state=2'b11;
+                    state<=2'b11;
                     cnt<=0;
                 end
                 else 
                     cnt<=cnt+1'b1;
         2'b10: if(cnt==32'b1_000_000) begin
-                    state=2'b11;
+                    state<=2'b11;
                     cnt<=0;
                 end
                 else
                     cnt<=cnt+1'b1;
         2'b00: cnt<=0;
-    endcase;
+    endcase
 end
 
-always@(posedge clk_50Hz) begin
-    if(turning==2'b01) begin
-        
-    end
-end
+
 always @(state,go_straight_command,turn_left_command,turn_right_command) begin
     case(state)
         2'b00: case({go_straight_command,turn_left_command,turn_right_command})
                     3'b100: 
-                        // {move_forward_signal,turn_left_signal,turn_right_signal}=3'b100;
                         state=2'b11;
                     3'b010: 
-                        // {move_forward_signal,turn_left_signal,turn_right_signal}=3'b010;
                         state=2'b01;
                     3'b001: 
-                        // {move_forward_signal,turn_left_signal,turn_right_signal}=3'b001;
                         state=2'b10;
                     // default: state=2'b00;
                 endcase
-        2'b01: //左转90度
+        2'b01: 
             {move_forward_signal,turn_left_signal,turn_right_signal}=3'b010;
-        // case({go_straight_command,turn_left_command,turn_right_command}):
-        //             3b'100: {move_forward_signal,turn_left_signal,turn_right_signal}=3b'100;
-        //             3b'010: {move_forward_signal,turn_left_signal,turn_right_signal}=3b'010;
-        //             3b'001: {move_forward_signal,turn_left_signal,turn_right_signal}=3b'001;
-        //         endcase;
         2'b10: 
             {move_forward_signal,turn_left_signal,turn_right_signal}=3'b001;
-        // case({go_straight_command,turn_left_command,turn_right_command}):
-                    // 3b'100: {move_forward_signal,turn_left_signal,turn_right_signal}=3b'100;
-                    // 3b'010: {move_forward_signal,turn_left_signal,turn_right_signal}=3b'010;
-                    // 3b'001: {move_forward_signal,turn_left_signal,turn_right_signal}=3b'001;
-                // endcase;
-        2'b11: {move_forward_signal,turn_left_signal,turn_right_signal}=3'b100;
+        2'b11: 
+            {move_forward_signal,turn_left_signal,turn_right_signal}=3'b100;
     endcase
-
 end
+
 
 endmodule
